f6319fbf1cac514544d5cb401c33fd80a480b0c0
Keith Brown <kbrownw@gmail.com>
2023-10-23 13:15:40 -0400

    Add loading spinner

--- src/components/ShowtimesLanding.jsx
+++ src/components/ShowtimesLanding.jsx
@@ -2,13 +2,21 @@ import { TitleLargeWhite } from "./TitleLargeWhite";
 import { useContext } from "react";
 import { ShowtimesContext } from "../context/ShowtimesProvider";
 import { MovieCard } from "./MovieCard/MovieCard";
+import { ThreeCircles } from "react-loader-spinner";
 
 export const ShowtimesLanding = () => {
   const { isLoading, showtimes, errorOccurred } = useContext(ShowtimesContext);
   const showtimesST = JSON.parse(sessionStorage.getItem("showtimes"));
 
   if (isLoading) {
-    return <TitleLargeWhite title="Loading..." classes="text-center" />;
+    return (
+      <>
+        <TitleLargeWhite title="Loading" classes="text-center" />
+        <div className="mx-auto">
+          <ThreeCircles height="100" color="#FFFFFF" />
+        </div>
+      </>
+    );
   } else {
     if (errorOccurred || !showtimesST) {
       return (
